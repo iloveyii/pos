6833b5d8f06e94789aa7395b25609044f81cb87a
Hazrat Ali <iloveyii@ayahoo.com>
2025-09-06 19:29:48 +0200

    fix: token is empty

--- src/main/java/net/softhem/pos/model/JwtFilter.java
+++ src/main/java/net/softhem/pos/model/JwtFilter.java
@@ -20,6 +20,9 @@ public class JwtFilter extends OncePerRequestFilter {
         String header = request.getHeader("Authorization");
         if (header != null && header.startsWith("Bearer ")) {
             String token = header.substring(7);
+            if(token.isEmpty()) {
+                return;
+            }
             String username = JwtUtil.getUsername(token);
 
             // Roles can also be parsed from token claims
