a8aac59c4232f16be0abfeb76e72f8559510c83a
Hazrat Ali <iloveyii@ayahoo.com>
2025-09-08 11:12:42 +0200

    fix: docker compose

--- latexnode/server.js
+++ latexnode/server.js
@@ -21,15 +21,15 @@ const server = http.createServer((req, res) => {
       try {
         const jsonData = JSON.parse(body);
         console.log('Received JSON:', jsonData); // Log to console
-        await compose_latex_main_file(jsonData.order);
-        await create_dir(jsonData.order);
-        const pdf_url = await generate_pdf(jsonData.order);
-        console.log('pdf_url:', pdf_url);
+        // await compose_latex_main_file(jsonData.order);
+        // await create_dir(jsonData.order);
+        // const pdf_url = await generate_pdf(jsonData.order);
+        // console.log('pdf_url:', pdf_url);
         
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({ 
           status: 'success', 
-          data: { url: pdf_url }
+          // data: { url: pdf_url }
         }));
       } catch (e) {
         res.writeHead(400, { 'Content-Type': 'application/json' });
@@ -42,12 +42,12 @@ const server = http.createServer((req, res) => {
   } else {
     // Handle 404 for other routes
     res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found\n');
+    res.end('404 Not Found - No route\n');
   }
 });
 
 // Start the server on port 3000
-const PORT = 3000;
+const PORT = 3001;
 const HOST = '0.0.0.0'; // Listen on all network interfaces
 server.listen(PORT, HOST, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
